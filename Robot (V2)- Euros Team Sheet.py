cc4d6caa0634196df63a4c21a0c35dd6aa632cfa
APang0371 <86012921+APang0371@users.noreply.github.com>
2021-08-13 11:02:40 +0100

    Create Robot (V2)- Euros Team Sheet.py
    Made changes to the original version. Neatened up the code lines. Also allows the user to search multiple countries without restarting the program.

---
+++ Robot (V2)- Euros Team Sheet.py
@@ -0,0 +1,105 @@
+Running=True
+print("Team Sheet for Each Countries First Euro 2020 Game")
+def printTeamSheet (TeamSheetList ):
+    for Item in TeamSheetList:
+            print(Item)
+Austria_team_sheet= ["Formation: 3511", "GK: Bachmann", "CB: Dragovic", "CB: Alaba (C)", "CB: Hinteregger", "RM: Lainer", "CM: Laimer", "CM: Baumgartner", "CM: Schlager", "LM: Ulmer", "CAM: Sabitzer", "ST: Kalajdzic"]
+
+Belgium_team_sheet= ["Formation: 343", "GK: Courtois",  "CB: Alderweireld", "CB: Boyata", "CB: Vertonghen (C)", "RM: Castagne", "CM: Dendoncker", "CM: Tielemans",  "LM: Hazard", "RW: Mertens,",  "ST: Lukaku", "LW: Cararasco"]
+
+Croatia_team_sheet=["Formation: 4141",  "GK: Livakovic",  "RB: Vrsaljiko", "CB: Vida", "CB: Calera-Car", "LB: Gvardiol", "CDM: Brozovic", "RM: Krmaric", "CM: Modric (C)", "CM: Kovacic", "LM: Perisic", "ST: Rebic"]
+
+Czech_Republic_team_sheet=["Formation: 4231", "GK: Vaclik", "RB: Coufal", "CB: Celustka", "CB: Kalas", "LB: Boril", "CM: Kral", "CM: Soucek", "RW: Masopust", "CAM: Darida (C)", "LW: Jankto", "ST: Schick"]
+
+Denmark_team_sheet=["Formation: 433", "GK: Schmeichel", "RB: Wass", "CB: Kjaer (C)", "CB: Christensen", "LB: Maehle", "CM: Eriksen", "CDM: Helbjerg",  "CM: Delaney",  "RW: Poulsen", "ST: Wind", "LW: Braithwaite"]
+
+England_team_sheet=["Formation: 4231", "GK: Pickford", "RB: Walker", "CB: Stones", "CB: Mings", "LB: Trippier", "CM: Phillips", "CM: Rice", "RW: Sterling", "CAM: Mount", "LW: Foden", "ST: Kane (C)"]
+
+Finland_team_sheet=["Formation: 352", "GK: Hradecky", "CB: Toivio", "CB: Arajuurl", "CB: O'Shaughnessy", "RM: Raitala", "CM: Lod", "CM: Sparv (C)", "CM: Kamara", "LM: Uronen", "ST: Pohjanpalo", "ST: Pukki"]
+
+France_team_sheet=["Formation: 4312", "GK: Lloris (C)", "RB: Pavard", "CB: Varane" , "CB: Kipembe", "LB: Hernandez", "CM: Rabiot", "CM: Kante", "CM: Pogba", "CAM: Griezmann", "ST: Benzema", "ST: Mbappe"]
+
+Germany_team_sheet=["Formation: 3421", "GK: Neuer (C)", "CB: Ginter", "CB: Hummels", "CB: Rudiger", "RM: Kimmich", "CM: Gundogan", "CM: Kroos", "LM: Gosens", "CAM: Muller", "CAM: Havertz", "ST: Gnabry"]
+
+Hungary_team_sheet=["Formation: 352", "GK: Gulacsi", "CB: Botka", "CB: Orban", "CB: Szalai", "RM: Lovrencsics", "CM: Kleinheisler", "CM: Nagy", "CM: Schafer", "LM: Fiola", "ST: Szalai (C)", "ST: Sallai"]
+
+Italy_team_sheet=["Formation: 433", "GK: Donnarumma", "RB: Florenzi", "CB: Bonnuci", "CB: Chiellini (C)", "LB: Spinazzola", "CM: Barella", "CM: Jorginho", "CM: Locatelli", "RW: Beradi", "ST: Immobile", "LW: Insigne"]
+
+Netherlands_team_sheet=["Formation: 352", "GK: Stekeenburg", "CB: Timber", "CB: De Vrij", "CB: Blind", "RM: Dumfries", "CM: De Roon", "CM: Wijnaldum (C)", "CM: De Jong", "LM: Van Anholt", "ST: Weghorts", "ST: Depay"]
+
+North_Macedonia_team_sheet=["Formation: 532", "GK: Dimitrievski", "RB: Nikolov", "CB: Ristovski", "CB: Velkovski", "CB: Musliu", "LB: Alioski", "CM: Bardhi", "CM: Ademi", "LM: Elmas", "ST: Pandev (C)", "ST: Trajkovski"]
+
+Poland_team_sheet=["Formation: 433", "GK: Szczesny", "RB: Bereszynski", "CB: Glik", "CB: Bednarek", "LB: Rybus", "CM: Linetty", "CM: Krychowiak", "CM: Klich", "RW: Jozwiak", "ST: Lewandowski (C)", "LW: Zielinski"]
+
+Portugal_team_sheet=["Formation: 4231", "GK: Patricio", "RB: Semedi", "CB: Pepe", "CB: Dias", "LB: Guerrerio", "CM: Pereira", "CM: Carvalho", "RW: Silva", "CAM: Fernandes", "LW: Jota", "ST: Ronaldo (C)"]
+
+Russia_team_sheet=["Formation: 4231", "GK: Shunin", "RB: Fernandes", "CB: Semenov", "CB: Dzhikiya", "LB: Zhirkov", "CM: Ozodoev", "CM: Barinov", "RW: Zobnin", "CAM: Golovin", "LW: Kuzyaev", "ST: Dzyuba (C)"]
+
+Scotland_team_sheet=["Formation: 3412", "GK: Marshall", "CB: Hendry", "CB: Hanley", "CB: Cooper", "RM: O'Donnell", "CM: McTominay", "CM: Armstrong", "LM: Robertson (C)", "CAM: McGinn", "ST: Dykes", "ST: Christie"]
+
+Slovakia_team_sheet=["Formation: 442", "GK: Dubravka", "RB: Pekarik", "CB: Satka", "CB: Skriniar", "LB: Hubocan", "RM: Haraslin", "CM: Kucka", "CM: Hromada", "LM: Mak", "ST: Duda", "ST: Hamsik (C)"]
+
+Spain_team_sheet=["Formation: 433", "GK: Simon", "RB: Llorente", "CB: Laporte", "CB: Torres", "LB: Alba (C)", "CM: Koke", "CM: Rodri", "CM: Pedri", "RW: Torres", "ST: Morata", "LW: Olmo"]
+
+Sweden_team_sheet=["Formation: 442", "GK: Olsen", "RB: Lustig", "CB: Lindelof", "CB: Danielson", "LB: Augustinsson", "RM: Larsson (C)", "CM: Ekdal", "CM: Olsson", "LM: Forsberg", "ST: Isak", "ST: Berg"]
+
+Switzerland_team_sheet=["Formation: 3412", "GK: Sommer", "CB: Elvedi", "CB: Schar", "CB: Akanji", "RM: Mbabu", "CM: Xhaka (C)", "CM: Freuler", "LM: Rodriguez", "CAM: Shaqiri", "ST: Embolo", "ST: Seferovic"]
+
+Turkey_team_sheet=["Formation: 4141", "GK: Cakir", "RB: Celik", "CB: Demiral", "CB: Soyuncu", "LB: Meral", "CDM: Okay Yokuslu", "RM: Karaman", "CM: Tufan", "CM: Yazici", "LM: Calhanoglu", "ST: Yilmaz (C)"]
+
+Ukraine_team_sheet=["Formation: 41221", "GK: Buschan", "RB: Karavaev", "CB: Zabarnyi", "CB: Matviyenki", "LB: Mykolenkil", "CDM: Zinchenko", "CM: Sydorchuk", "CM: Malinovsky", "CAM: Yarmolenko (C)", "CAM: Zubkov", "ST: Yaremchuk"]
+
+Wales_team_sheet=["Formation: 433", "GK: Ward", "RB: Roberts", "CB: Mepham", "CB: Rodon", "LB: Davies", "CM: Morrell", "CM: Allen", "CM: Ramsey", "RW: Bale (C)", "ST: Moore", "LW: James"]
+
+while  Running:
+    input1=input("Type for a country and their lineup, or type Stop to quit")
+    if input1== ("Austria"):
+        printTeamSheet(Austria_team_sheet)
+    elif input1== ("Belgium"):
+        printTeamSheet(Belgium_team_sheet)
+    elif input1== ("Croatia"):
+        printTeamSheet(Croatia_team_sheet)
+    elif input1== ("Czech Republic"):
+        printTeamSheet(Czech_Republic_team_sheet)
+    elif input1== ("Denmark"):
+        printTeamSheet(Denmark_team_sheet)
+    elif input1== ("England"):
+        printTeamSheet(England_team_sheet)
+    elif input1== ("Finland"):
+        printTeamSheet(Finland_team_sheet)
+    elif input1== ("France"):
+        printTeamSheet(France_team_sheet)
+    elif input1== ("Germany"):
+        printTeamSheet(Germany_team_sheet)
+    elif input1== ("Hungary"):
+        printTeamSheet(Hungary_team_sheet)
+    elif input1== ("Italy"):
+         printTeamSheet(Italy_team_sheet)
+    elif input1== ("Netherlands"):
+        printTeamSheet(Netherlands_team_sheet)
+    elif input1== ("North Macedonia"):
+        printTeamSheet(North_Macedonia_team_sheet)
+    elif input1== ("Poland"):
+        printTeamSheet(Poland_team_sheet)
+    elif input1== ("Portugal"):
+        printTeamSheet(Portugal_team_sheet)
+    elif input1== ("Russia"):
+        printTeamSheet(Russia_team_sheet)
+    elif input1== ("Scotland"):
+        printTeamSheet(Scotland_team_sheet)
+    elif input1== ("Slovakia"):
+        printTeamSheet(Slovakia_team_sheet)
+    elif input1== ("Spain"):
+        printTeamSheet(Spain_team_sheet)
+    elif input1== ("Sweden"):
+        printTeamSheet(Swede_team_sheet)
+    elif input1== ("Switzerland"):
+        printTeamSheet(Switzerland_team_sheet)
+    elif input1== ("Turkey"):
+        printTeamSheet(Switzerland_team_sheet)
+    elif input1== ("Ukraine"):
+        printTeamSheet(Ukraine_team_sheet)
+    elif input1== ("Wales"):
+        printTeamSheet(Wales_team_sheet)
+    elif input1=="Stop":
+        Running=False
